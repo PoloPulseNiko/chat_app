194bab481c04b892b270bc9048dfc8ecf2f91546
PoloPulseNiko <nikolaypetouhoff@gmail.com>
2026-04-17 13:44:31 +0200

    switched from Postgres to SQLite for testing

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -84,14 +84,21 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME", "chat_app"),
+#         "USER": os.getenv("DB_USER", "postgres-user"),
+#         "PASSWORD": os.getenv("DB_PASSWORD", "password"),
+#         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+#         "PORT": os.getenv("DB_PORT", "5432"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "chat_app"),
-        "USER": os.getenv("DB_USER", "postgres-user"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "password"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
