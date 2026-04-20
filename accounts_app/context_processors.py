c5894aa673b29a6666ff1c89ee026cf074f3c4cc
PoloPulseNiko <nikolaypetouhoff@gmail.com>
2026-04-20 12:59:48 +0200

    More fix attempts for the homepage

--- accounts_app/context_processors.py
+++ accounts_app/context_processors.py
@@ -1,5 +1,6 @@
 from .services import ensure_user_profile
 
-
 def current_profile(request):
+    if not request.user.is_authenticated:
+        return {"current_profile": None}
     return {"current_profile": ensure_user_profile(request.user)}
